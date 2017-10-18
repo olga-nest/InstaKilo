fae0ebb84a9e22766cbdd09f8643af4b7eaacff7
olga-nest <olga.w.nesterova@gmail.com>
2017-10-18 15:06:49 -0700

    Add header class, add label outlet

---
+++ InstaKilo/ONEHeaderCollectionReusableView.m
@@ -0,0 +1,13 @@
+//
+//  ONEHeaderCollectionReusableView.m
+//  InstaKilo
+//
+//  Created by Olga on 10/18/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import "ONEHeaderCollectionReusableView.h"
+
+@implementation ONEHeaderCollectionReusableView
+
+@end
