8f83aa435ec6d014993feed620eaa02836727d5c
olga-nest <olga.w.nesterova@gmail.com>
2017-10-18 12:08:06 -0700

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Olga on 10/18/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
