3092bd5837b4d3d4bbef36c5e15c32f262812aa9
olga-nest <olga.w.nesterova@gmail.com>
2017-10-18 14:01:51 -0700

    Setup default layout, show images

--- InstaKilo/ViewController.m
+++ InstaKilo/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "ONECollectionViewCell.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDataSource>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@property (nonatomic, strong) UICollectionViewFlowLayout *defaultLayout;
+
+@property (nonatomic, strong) NSArray *imagesArr;
 
 @end
 
@@ -16,14 +22,72 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.collectionView.dataSource = self;
+    [self createImagesArray];
+    [self setupDefaultLayout];
+    
+    self.collectionView.collectionViewLayout = self.defaultLayout;
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)setupDefaultLayout {
+        self.defaultLayout = [[UICollectionViewFlowLayout alloc] init];
+        
+        self.defaultLayout.itemSize = CGSizeMake(100, 100); // Set size of cell
+        self.defaultLayout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10);  // "Border around each section"
+        self.defaultLayout.minimumInteritemSpacing = 15;  // Minimum horizontal spacing between cells
+        self.defaultLayout.minimumLineSpacing = 10;  // Minimum vertical spacing
+        
+        // By default, direction is vertical
+        //self.simpleLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+        
+        // Add this line so headers will appear. If this line is not present, headers will not appear
+        //self.simpleLayout.headerReferenceSize = CGSizeMake(50, self.collectionView.frame.size.height);
+        
+        // Add this line so footers will appear. If this line is not present, footers will not appear
+        //self.simpleLayout.footerReferenceSize = CGSizeMake(30, self.collectionView.frame.size.height);
+    
 }
 
+
+
+-(void)createImagesArray {
+//    if (!self.imagesArr) {
+//    NSArray *imagesArr =[NSArray new];
+//    }
+    
+    NSMutableArray *tempArr = [[NSMutableArray alloc] init];
+    for (int i = 1; i <= 13; i++) {
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%i", i]];
+        [tempArr addObject:image];
+    }
+    
+    self.imagesArr = tempArr.copy;
+    NSLog(@"Objects in imagesArr: %lu", (unsigned long)self.imagesArr.count);
+    
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section {
+    return 13;
+}
+
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    ONECollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"imageId" forIndexPath:indexPath];
+    
+   // NSInteger *index = indexPath.row;
+    
+    cell.imageView.image = [self.imagesArr objectAtIndex:indexPath.row];
+    
+    
+    return cell;
+}
 
 @end
