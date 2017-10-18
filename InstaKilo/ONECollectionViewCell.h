1d41861483f4526f4f79b15b20117565da1f6929
olga-nest <olga.w.nesterova@gmail.com>
2017-10-18 12:27:27 -0700

    Add ONECollectionViewCell class, connect cell image outlet

---
+++ InstaKilo/ONECollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  ONECollectionViewCell.h
+//  InstaKilo
+//
+//  Created by Olga on 10/18/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ONECollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ InstaKilo/ONECollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  ONECollectionViewCell.m
+//  InstaKilo
+//
+//  Created by Olga on 10/18/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import "ONECollectionViewCell.h"
+
+@implementation ONECollectionViewCell
+
+@end
